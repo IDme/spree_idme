22ed9fd66555099ca47919c9b037844a70199920
Chris Morris <ckmorris@ckmorris.net>
2013-10-29 21:22:40 -0400

    remove unneeded sandbox setting

--- db/migrate/20131022013301_add_idme_setup_to_spree_promotion_rules.rb
+++ db/migrate/20131022013301_add_idme_setup_to_spree_promotion_rules.rb
@@ -2,6 +2,5 @@ class AddIdmeSetupToSpreePromotionRules < ActiveRecord::Migration
   def change
     add_column :spree_promotion_rules, :spree_idme_setting_id,  :integer, :default => 1
     add_column :spree_promotion_rules, :idme_affinity, :string
-    add_column :spree_promotion_rules, :idme_sandbox,  :boolean
   end
 end
