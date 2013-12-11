0097acb35cf5e8b25392447a664af885fbf5996e
Chris Morris <christopher@troopswap.com>
2013-12-11 16:46:54 -0500

    remove unneeded attributes

--- db/migrate/20131025175419_create_spree_idme_settings.rb
+++ db/migrate/20131025175419_create_spree_idme_settings.rb
@@ -4,14 +4,10 @@ class CreateSpreeIdmeSettings < ActiveRecord::Migration
       t.string   :idme_client_id_string
       t.string   :idme_client_secret
       t.string   :redirect_uri
-      t.string   :military_button
-      t.string   :student_button
-      t.string   :responder_button
       t.boolean  :idme_sandbox
       t.boolean  :idme_modal
       t.datetime :affiliations_updated_at
       t.timestamps
     end
-    SpreeIdmeSetting.create!({:military_button => "https://s3.amazonaws.com/idme/buttons/troop_red_squ_40.png", :student_button => "https://s3.amazonaws.com/idme/buttons/student_gray2_squ_40.png", :responder_button => "https://s3.amazonaws.com/idme/buttons/responder_gray2_squ_40.png"}, :without_protection => true)
   end
 end
